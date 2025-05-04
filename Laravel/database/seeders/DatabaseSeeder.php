21b08d552d500122146a688c31974047f0938e4c
Dhruv Patel <1663dhruv@gmail.com>
2025-05-04 11:20:07 +0530

    Categories Done

--- Laravel/database/seeders/DatabaseSeeder.php
+++ Laravel/database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PromoCodeSeeder::class
         ]);
+        
         $this->call([
             CompleteDataSeeder::class,
         ]);
